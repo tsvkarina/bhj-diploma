e9bd6e121b7686339f8cd9c44c6690512a8f94ce
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 21:39:45 +0300

    Update UserWidget -1.js

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -3,26 +3,26 @@
  * отображение информации о имени пользователя
  * после авторизации или его выхода из системы
  * */
-
 class UserWidget {
-  /**
-   * Устанавливает полученный элемент
-   * в свойство element.
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor(element){
 
+  constructor(element) {
+    if (!element) {
+      throw new Error('Передан пустой элемент в UserWidget');
+    }
+
+    this.element = element;
   }
 
-  /**
-   * Получает информацию о текущем пользователе
-   * с помощью User.current()
-   * Если пользователь авторизован,
-   * в элемент .user-name устанавливает имя
-   * авторизованного пользователя
-   * */
-  update(){
+  update() {
+    const currentUser = User.current();
 
+    if (currentUser) {
+      const userNameElement = this.element.querySelector('.user-name');
+      if (userNameElement) {
+        userNameElement.textContent = currentUser.name;
+      }
+    }
   }
 }
+
+export default UserWidget;
