2a37f09cf210977c05761dd2924093f02b41c2ce
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 22:02:48 +0300

    Update CreateTransactionForm -1.js

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -3,29 +3,33 @@
  * создания новой транзакции
  * */
 class CreateTransactionForm extends AsyncForm {
-  /**
-   * Вызывает родительский конструктор и
-   * метод renderAccountsList
-   * */
   constructor(element) {
-    super(element)
+    super(element);
+    this.renderAccountsList();
   }
 
-  /**
-   * Получает список счетов с помощью Account.list
-   * Обновляет в форме всплывающего окна выпадающий список
-   * */
   renderAccountsList() {
+    Account.list(User.current(), (err, response) => {
+      if (response && response.success) {
+        const accountsSelect = this.element.querySelector('.accounts-select');
+        accountsSelect.innerHTML = '';
 
+        response.data.forEach(account => {
+          accountsSelect.insertAdjacentHTML('beforeend', `
+            <option value="${account.id}">${account.name}</option>
+          `);
+        });
+      }
+    });
   }
 
-  /**
-   * Создаёт новую транзакцию (доход или расход)
-   * с помощью Transaction.create. По успешному результату
-   * вызывает App.update(), сбрасывает форму и закрывает окно,
-   * в котором находится форма
-   * */
   onSubmit(data) {
-
+    Transaction.create(data, (err, response) => {
+      if (response && response.success) {
+        this.element.reset();
+        App.getModal(this.element.closest('.modal')).close();
+        App.update();
+      }
+    });
   }
 }
