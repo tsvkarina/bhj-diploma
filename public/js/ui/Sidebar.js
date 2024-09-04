183ee3c327ca28588cae7817157ae2eea0324959
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 20:57:12 +0300

    Update Sidebar -1.js

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -4,31 +4,31 @@
  * и за кнопки меню
  * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
+    const toggleButton = document.querySelector('.sidebar-toggle');
 
+    if (toggleButton) {
+      toggleButton.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const body = document.querySelector('body');
+
+        if (body.classList.contains('sidebar-open')) {
+          body.classList.remove('sidebar-open');
+          body.classList.add('sidebar-collapse');
+        } else {
+          body.classList.remove('sidebar-collapse');
+          body.classList.add('sidebar-open');
+        }
+      });
+    }
   }
 
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
   static initAuthLinks() {
-
   }
 }
