afcd1d0c53eaef513e239085d08480aac2ec6997
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 21:05:00 +0300

    Update Modal -1.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -5,43 +5,33 @@
  * закрытие имеющихся окон
  * */
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor(element){
 
+  constructor(element) {
+    if (!element || !(element instanceof HTMLElement)) {
+      throw new Error('Invalid element provided to Modal constructor.');
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
   registerEvents() {
-
+    const closeButtons = this.element.querySelectorAll('[data-dismiss="modal"]');
+    
+    closeButtons.forEach(button => {
+      button.addEventListener('click', (e) => this.onClose(e));
+    });
   }
 
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
   onClose(e) {
-
+    e.preventDefault();
+    this.close();
   }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
+
   open() {
-
+    this.element.style.display = 'block';
   }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
 
+  close() {
+    this.element.style.display = '';
   }
 }
