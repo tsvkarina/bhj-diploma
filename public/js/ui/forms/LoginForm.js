c55f0492c67af864625f08f572d09c654061064d
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 21:35:24 +0300

    Update LoginForm -1.js

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -3,13 +3,24 @@
  * входа в портал
  * */
 class LoginForm extends AsyncForm {
-  /**
-   * Производит авторизацию с помощью User.login
-   * После успешной авторизации, сбрасывает форму,
-   * устанавливает состояние App.setState( 'user-logged' ) и
-   * закрывает окно, в котором находится форма
-   * */
-  onSubmit(data) {
 
+  onSubmit(data) {
+    User.login(data, (response) => {
+      if (response.success) {
+
+        this.element.reset();
+
+        App.setState('user-logged');
+
+        const modal = this.element.closest('.modal');
+        if (modal) {
+          Modal.close(modal);
+        }
+      } else {
+        console.error(response.error || 'Ошибка авторизации');
+      }
+    });
   }
 }
+
+export default LoginForm;
