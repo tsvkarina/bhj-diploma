4b1f99892eea071b75d3b962fca20f75f78d7f6d
tsvkarina <149406375+tsvkarina@users.noreply.github.com>
2024-09-04 20:09:37 +0300

    Update createRequest -1.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,36 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  let url = options.url;
+  
+  if (options.method === 'GET') {
+    const params = new URLSearchParams(options.data).toString();
+    url += `?${params}`;
+  }
 
+  xhr.open(options.method, url);
+  xhr.responseType = 'json';
+
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      options.callback(null, xhr.response);
+    } else {
+      options.callback(xhr.response, null);
+    }
+  };
+
+  xhr.onerror = () => {
+    options.callback(new Error('Network Error'), null);
+  };
+
+  if (options.method !== 'GET') {
+    const formData = new FormData();
+    for (let key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+    xhr.send(formData);
+  } else {
+    xhr.send();
+  }
 };
